789505e7b7ce0f94589a3f048ae03848dfb4491f
Hieu Truong Xuan <truongxuanhieu11@gmail.com>
2026-02-10 03:20:17 +0000

    rerun cplex without runlim

--- other-approaches/CP-CPLEX/run.sh
+++ other-approaches/CP-CPLEX/run.sh
@@ -4,10 +4,7 @@ chmod +x runlim
 
 for d in 40 60 80 100; do
   for i in $(seq 1 10); do
-    ./runlim -r 600 -s 5000 \
-      -o runlim_log/NRP_1_${d}_run${i}.log \
-      oplrun model/NRP.mod data/NRP_1_${d}.input 2>&1 \
-      | tee cplex_log/NRP_1_${d}_run${i}.out
+    oplrun model/NRP.mod data/NRP_1_${d}.input 2>&1 | tee cplex_log/NRP_1_${d}_run${i}.out
   done
 done
 
